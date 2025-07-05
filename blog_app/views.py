1b442dfd4ea7aada5c5f1a2b7d3e2fee46951e55
Andrey <kaspersky1236@gmail.com>
2025-07-06 00:49:23 +0400

    Work with Templates & Static. Finish Frontend.

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Andrey',
+        'title': 'Title of Andrey\'s post.',
+        'date': '05.07.2025',
+        'content': 'SOME CONTENT',
+    },
+    {
+        'author': 'Walter',
+        'title': 'Title of Walter\'s post.',
+        'date': '06.09.2069',
+        'content': 'SOME CONTENT',
+    }
+]
+
 def Home(request):
-    return render(request, "blog_app/home.html")
+    
+    context = {
+        'var_posts': posts,
+        'title': 'Home',
+    }
+    
+    return render(request, 'blog_app/home.html', context)
+
+def About(request):
+    return render(request, 'blog_app/about.html', {
+        'title': 'About Us',
+    })
