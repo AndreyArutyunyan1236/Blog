dbe795cd77667220f7846406c69cdfc85005e3ed
Andrey <kaspersky1236@gmail.com>
2025-07-11 23:11:19 +0400

    Created POST method to user registration. Sing in link created.

--- users_app/views.py
+++ users_app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 def reg(request):
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, f'Account created for {username}!')
+            return redirect()
+        
     form = UserCreationForm()
+    
     return render(request, 'users_app/reg.html', { 'form': form })
+
+# messages.debug()
+# messages.info()
+# messages.success()
+# messages.warning()
+# messages.error()
